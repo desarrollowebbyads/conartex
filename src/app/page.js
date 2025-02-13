fd6678bf3efee5b6d19395a228e64e4a1073952f
Desarrollowebbyads <jeronimo@byads.co>
2025-02-13 16:22:10 -0500

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,153 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div>
+      <Head>
+        <title>CONARTEX - Calidad y Confianza</title>
+        <meta name="description" content="10 años transformando ideas en prendas de calidad." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+      <header className="nav_bar">
+        <div className="nav_bartitle">
+          <h1>Conar<b>tex</b><b>S.A.S</b></h1>
         </div>
+        <div className="nav_barredes">
+          <Image
+          src={"/Recurso_2.svg"}
+          width={30} 
+          height={30} 
+          alt="Contartex_facebook" />
+          <Image
+          src={"/Recurso_3.svg"}
+          width={30} 
+          height={30} 
+          alt="Contartex_instagram"/>
+        </div>
+      </header>
+
+      <main className="body">
+        <section className="Section1">
+          <div className="section1_image">
+            <Image
+            src={"/Recurso-21.webp"}
+            width={700} 
+            height={800} 
+            alt="Contartex" />
+          </div>
+
+          <div className="section1_s">
+              <div className="section1_info">
+                <h2>Llevamos una decada en el mercado</h2>
+                <p className="section1_text1">confeccionando calidad y confianza</p>
+              </div>
+              <div className="section1_info2">
+                <p className="section1_text2">10 años transformando ideas en prendas de calidad.Somos expertos en confección de ropa en tejido de punto para hombres, mujeres y niños. </p>
+                <p className="section1_text3"><b>Nuestro compromiso </b>con grandes marcas nos ha convertido en aliados confiables para ofrecer paquetes completos de producción.</p>
+              </div>
+          </div>
+          
+        </section>
+        <section className="section_tex">
+        <Image
+            src={"/Recurso-22.webp"}
+            width={750} 
+            height={220} 
+            alt="Contartex" />
+        </section>
+
+        <section className="section2">
+        <Image
+          src={"/Recurso_4.svg"}
+          width={60} 
+          height={60} 
+          alt="Contartex_icon1"/>
+          <Image
+          src={"/Recurso_5.svg"}
+          width={60} 
+          height={60} 
+          alt="Contartex_icon2"/>
+          <Image
+          src={"/Recurso_6.svg"}
+          width={60} 
+          height={60} 
+          alt="Contartex_icon3"/>
+          <p>100%</p>
+          <h3 className="section2_">Hecho en Mde.</h3>
+          <Image
+          src={"/Recurso_7.svg"}
+          width={600} 
+          height={200} 
+          alt="Contartex_barras"/>
+        </section>
+
+      
+        <section className="section3">
+          <div className="section3_efi">
+          <h3 >Eficiencia</h3>
+          <p >Trabajamos con procesos optimizados para garantizar resultados.</p></div>
+          <div className="section3_cali">
+          <h3 >Calidad Inigualable</h3>
+          <p>Cada prenda pasa por un estricto control para superar tus expectativas.</p>
+          </div>
+          <div className="section_entre">
+          <h3 >Entregas a Tiempo</h3>
+          <p>Cumplimos siempre con los plazos acordados.</p></div>
+        </section>
+        <section className="sectionimage">
+          <Image
+          layoout="responsive"
+          src={"/Recurso-24.webp"}
+          width={1724} 
+          height={800} 
+          alt="Contartex_barras"/>
+        </section>
+
+        <section className="section4">
+          <div className="section4_ns">
+               <Image
+                  src={"/Recurso_8.svg"}
+                  width={800} 
+                  height={500} 
+                  alt="Contartex_barras"/>
+                <h2>NUESTROS SERVICIOS</h2>
+          </div>
+          <div className="section4_text">
+            <p>Nos especializamos en la confección de prendas en tejido de punto, ofreciendo calidad, precisión y durabilidad en cada diseño. </p>
+            <p>Nuestro enfoque está en transformar cada hilo en piezas cómodas, versátiles y de alto rendimiento, adaptándonos a las necesidades del mercado con excelencia y compromiso.</p>
+          </div>
+        </section>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+      <footer>
+  
+  <div className="footer-section footer-left">
+    <h2>Conartex <span>S.A.S</span></h2>
+    <p>Copyright © 2025</p>
+    <p>Conartex S.A.S.</p>
+    <p>Todos los derechos reservados.</p>
+  </div>
+
+  
+  <div className="footer-section footer-center">
+    <div className="footer-icons">
+      <Image src="/Recurso-2.svg" width={30} height={30} alt="Facebook" />
+      <Image src="/Recurso-3.svg" width={30} height={30} alt="Instagram" />
+    </div>
+    <h3>Contáctanos</h3>
+    <p>Carrera 50 # 25-127</p>
+    <p>4° piso</p>
+    <p>Medellín, Colombia.</p>
+  </div>
+
+  
+  <div className="footer-section footer-right">
+    <p>conartexsas@hotmail.com</p>
+  </div>
+</footer>
     </div>
   );
-}
+
+  }
